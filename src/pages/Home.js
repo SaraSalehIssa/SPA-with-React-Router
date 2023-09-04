d80a44215de27264d9396ce787b1b1ce01553de0
SaraSalehIssa <issasalehsara2001@gmail.com>
2023-09-04 15:58:12 +0300

    Building a Multi-Page SPA with React Router

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
-function HomePage(){
-    return <h1>This is home page</h1>
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+    return <>
+        <h1>This is home page</h1>
+        <p>GO TO <Link to="/products">list of products</Link>.</p>
+    </>
 }
 
 export default HomePage;
